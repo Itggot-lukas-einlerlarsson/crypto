7c1d357b4d6530c7b2b753ff8f884314cec4b833
Lukas Einler Larsson <Lukas.einlerlarsson@itggot.se>
2021-12-29 15:52:39 +0100

    Update prime_discovery.cpp

--- prime_discovery.cpp
+++ prime_discovery.cpp
@@ -91,6 +91,6 @@ void getPrimeVector(std::vector<int> &primeVector, const int& max){
 
 //futher ideas - probably nonsense
 //a^2 = (a-1)(a+1)+1
-//we have formulas, ex https://en.wikipedia.org/wiki/Formula_for_primes
+//https://en.wikipedia.org/wiki/Formula_for_primes
   // Dynamic, moving formulas? parallell formulas, a table of formulas depending on indices(nth_prime)
-  // moving modulus-formula,  "interplay of patterns", 
+  // moving modulus-formula, interplay
